681549e7300206bf600fd1d9ccd8cc8076523b90
Fran Jurmanović <fjurma12@outlook.com>
2021-05-25 20:22:06 +0200

    removed fatal env file error

--- cmd/migrate/main.go
+++ cmd/migrate/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"log"
 	"os"
 	"wallet-api/pkg/migrate"
 	"wallet-api/pkg/utl/db"
@@ -10,11 +9,7 @@ import (
 )
 
 func main() {
-
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
+	godotenv.Load()
 
 	dbUrl := os.Getenv("DATABASE_URL")
 
